13fff30f8e42c206f1c20ccc63015971c40d4354
Chris Tosswill <ctosswill@engineyard.com>
2011-10-13 15:40:06 -0700

    made urls abs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  
+  def abs_img( name )
+    request.protocol + request.host_with_port + '/images/' + name
+  end
+  helper_method :abs_img
 end
